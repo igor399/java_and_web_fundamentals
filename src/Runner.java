644f80a58367432a1adede7e2594e12dc22bd602
Igor <88189374+igor399@users.noreply.github.com>
2022-02-21 12:49:11 +0300

    some refactoring

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,14 @@
+import by.epam.lab.Material;
+import by.epam.lab.Subject;
+
 public class Runner {
+    public static void main(String[] args) {
+        final Material STEEL = new Material("steel", 7850.0);
+        Subject wire = new Subject("wire", STEEL, 0.03);
+        System.out.println(wire);
+
+        final Material COOPER = new Material("cooper", 8500.0);
+        wire.setMaterial(COOPER);
+        System.out.println("The wire mass is " + wire.getMass() + "kg.");
+    }
 }
