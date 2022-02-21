dc3f06e79470d16f06fbac3b68640e8716e607d1
Igor <88189374+igor399@users.noreply.github.com>
2022-02-21 12:57:22 +0300

    some refactoring

--- src/Runner.java
+++ src/Runner.java
@@ -6,9 +6,8 @@ public class Runner {
         final Material STEEL = new Material("steel", 7850.0);
         Subject wire = new Subject("wire", STEEL, 0.03);
         System.out.println(wire);
-
-        final Material COOPER = new Material("cooper", 8500.0);
-        wire.setMaterial(COOPER);
+        final Material COPPER = new Material("copper", 8500.0);
+        wire.setMaterial(COPPER);
         System.out.println("The wire mass is " + wire.getMass() + "kg.");
     }
 }
