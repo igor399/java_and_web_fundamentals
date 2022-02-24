4c84c9a3302ef173e0f05b7020031e7a7f5e56f3
Igor <88189374+igor399@users.noreply.github.com>
2022-02-24 21:04:16 +0300

    some refactoring

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,9 @@
 import by.epam.lab.Purchase;
+import by.epam.lab.WeekDay;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,28 +13,58 @@ public class Runner {
             sc.useLocale(Locale.ENGLISH);
 
             final int PURCHASES_NUMBER = sc.nextInt();
-            Purchase[ ] purchases = new Purchase[PURCHASES_NUMBER];
+            Purchase[] purchases = new Purchase[PURCHASES_NUMBER];
 
-            for(int i = 0; i < purchases.length; i++){
-                purchases[i] = new Purchase(sc.nextInt(), sc.nextDouble(),sc.nextInt());
+            for (int i = 0; i < purchases.length; i++) {
+                purchases[i] = new Purchase(sc.nextInt(), sc.nextDouble(), sc.nextInt());
             }
 
             printArrayContent(purchases);
 
+            int totalCost = 0;
+            double meanCost = 0.0;
+            int totalCostMonday = 0;
+            int maxCost = 0;
+            WeekDay maxPurchaceWeekday = null;
 
+            for (Purchase purchase : purchases) {
+                int cost = purchase.getCost();
+                totalCost += cost;
+                if (purchase.getWeekDay() == WeekDay.MONDAY) {
+                    totalCostMonday += cost;
+                }
+                if (cost > maxCost) {
+                    maxCost = cost;
+                    maxPurchaceWeekday = purchase.getWeekDay();
+                }
+            }
+            if (purchases.length > 0) {
+                meanCost = (double) totalCost / purchases.length / 100;
+            }
+            System.out.println("Mean cost = " + String.format("%d.%02d", meanCost));
+            System.out.println("Total cost on Mondays = " + Purchase.currencyConvention(totalCostMonday));
+            System.out.println("The day with the max purchace is " + maxPurchaceWeekday);
+
+            Arrays.sort(purchases);
+            printArrayContent(purchases);
+
+            Purchase equalledPurchace = new Purchase(5, 0, null);
+            int equalledPurchaceIndex = Arrays.binarySearch(purchases, equalledPurchace);
+            if (equalledPurchaceIndex >= 0) {
+                System.out.println("Equalled purchace :" + purchases[equalledPurchaceIndex]);
+            } else {
+                System.out.println("Equalled purchace is't found.");
+            }
         } catch (FileNotFoundException e) {
             System.err.println("Input file is not found");
         }
-
-
     }
 
-    private static void printArrayContent(Purchase[]purchases){
+    private static void printArrayContent(Purchase[] purchases) {
         System.out.println("product name " + Purchase.PRODUCT_NAME);
         System.out.println("price = " + Purchase.currencyConvention(Purchase.PRICE));
-        for(Purchase purchase : purchases){
+        for (Purchase purchase : purchases) {
             System.out.println(purchases);
         }
     }
-
 }
