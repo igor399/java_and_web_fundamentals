16e56f5b54f26813253fecffbea0e3e220e445a5
Igor <88189374+igor399@users.noreply.github.com>
2022-02-28 11:32:37 +0300

    some ref

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -14,20 +14,14 @@ public class TestRunner {
 
     @Test
     public void testGetCost() {
-        Purchase p1 = new Purchase(1, 10.3, 0);
+        Purchase p1 = new Purchase(1, 10, 0);
         Assert.assertEquals(200, p1.getCost());
-        Purchase p2 = new Purchase(10, 15.8, 1);
+        Purchase p2 = new Purchase(10, 15.9, 1);
         Assert.assertEquals(2100, p2.getCost());
-        Purchase p3 = new Purchase(5, 43, 2);
+        Purchase p3 = new Purchase(5, 43.5, 2);
         Assert.assertEquals(700, p3.getCost());
-        Purchase p4 = new Purchase(75, 71.2, 3);
-        Assert.assertEquals(5400, p4.getCost());
-        Purchase p5 = new Purchase(95, 63.8, 4);
-        Assert.assertEquals(8600, p5.getCost());
-        Purchase p6 = new Purchase(16, 37.7, 5);
-        Assert.assertEquals(2500, p6.getCost());
-        Purchase p7 = new Purchase(35, 13.9, 6);
-        Assert.assertEquals(7500, p7.getCost());
+        Purchase p4 = new Purchase(75, 71.8, 3);
+        Assert.assertEquals(5300, p4.getCost());
     }
 
     @Test
