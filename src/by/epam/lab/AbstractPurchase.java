498ad3cd788e85ae98d73a96619a5a1df7ef7f92
Igor <88189374+igor399@users.noreply.github.com>
2022-03-14 13:53:46 +0300

    some ref

---
+++ src/by/epam/lab/AbstractPurchase.java
@@ -0,0 +1,49 @@
+package by.epam.lab;
+
+public abstract class AbstractPurchase implements Comparable<AbstractPurchase> {
+    private Product product;
+    private int numUnits;
+
+    public AbstractPurchase() {
+        this(null, 0);
+    }
+
+    public AbstractPurchase(Product product, int numUnits) {
+        this.product = product;
+        this.numUnits = numUnits;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getNumUnits() {
+        return numUnits;
+    }
+
+    public void setNumUnits(int numUnits) {
+        this.numUnits = numUnits;
+    }
+
+    protected abstract Byn getNotRoundCost(Byn byn);
+
+    public void getCost() {
+        Byn baseCost = product.getPrice().multiply(numUnits);
+        Byn finalCost = getNotRoundCost(baseCost);
+        return finalCost.round(RoundMethod.FLOOR, 2);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "; " + numUnits + "; " + getCost();
+    }
+
+    @Override
+    public int compareTo(AbstractPurchase o) {
+        return getCost().compareTo(o.getCost());
+    }
+}
