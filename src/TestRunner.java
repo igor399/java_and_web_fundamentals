82cd00eab31c7fdbacda4f7cceac9e76c220b6b0
Igor <88189374+igor399@users.noreply.github.com>
2022-03-20 20:33:54 +0300

    some refactoring

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,10 +1,12 @@
-import java.io.File;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-public class Runner {
+public class TestRunner {
     public static void main(String[] args) throws FileNotFoundException {
         final String CSV_NAME = "src/in1.csv";
         final String ERROR_LINES = "error-lines = ";
@@ -53,5 +55,50 @@ public class Runner {
         }
         strResult.insert(0, RESULT_HEAD).append(RESULT_TAIL).append(numResult);
         return errorsNumb;
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void testNoFile() throws FileNotFoundException {
+        int errorLine = getResult("src/in9.csv", new StringBuilder());
+    }
+
+    @Test
+    public void getResultFirst() throws FileNotFoundException {
+        StringBuilder strResult = new StringBuilder();
+        int errorLine = getResult("src/in1.csv", new StringBuilder());
+        Assert.assertEquals(3, errorLine);
+        Assert.assertEquals("result(5.2 - 3.14 + 0.0) = 2.06", strResult.toString());
+    }
+
+    @Test
+    public void getResultSecond() throws FileNotFoundException {
+        StringBuilder strResult = new StringBuilder();
+        int errorLine = getResult("src/in2.csv", strResult);
+        Assert.assertEquals(0, errorLine);
+        Assert.assertEquals("result(-3.1 - 1.0) = -4.1", strResult.toString());
+    }
+
+    @Test
+    public void getResultThird() throws FileNotFoundException {
+        StringBuilder strResult = new StringBuilder();
+        int errorLine = getResult("src/in3.csv", strResult);
+        Assert.assertEquals(0, errorLine);
+        Assert.assertEquals("result(0.75) = 0.75", strResult.toString());
+    }
+
+    @Test
+    public void getResultFour() throws FileNotFoundException {
+        StringBuilder strResult = new StringBuilder();
+        int errorLine = getResult("src/in4.csv", strResult);
+        Assert.assertEquals(0, errorLine);
+        Assert.assertEquals("result(0.0) = 0.0", strResult.toString());
+    }
+
+    @Test
+    public void getResultFive() throws FileNotFoundException {
+        StringBuilder strResult = new StringBuilder();
+        int errorLine = getResult("src/in5.csv", strResult);
+        Assert.assertEquals(1, errorLine);
+        Assert.assertEquals("result() = 0.0", strResult.toString());
     }
 }
