33402e23ff4ff92c460d2cb9308aa8c2e6dc01e4
Igor <88189374+igor399@users.noreply.github.com>
2022-03-28 13:53:04 +0300

    some ref

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -8,82 +8,75 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TestRunner {
-    final static String RESULT = "sum = ";
+    static final String RESULT = "sum = ";
 
     private static int getResult(String propName, StringBuilder strResult) {
-        double numResult = 0.0;
-        int errorLines = 0;
         ResourceBundle rb = ResourceBundle.getBundle(propName);
         Enumeration<String> keys = rb.getKeys();
+        final String KEY_REG_EXP = "index(.*)";
+        final String NUM_REG_EXP = "[1-9]\\d*";
+        Pattern keyPattern = Pattern.compile(KEY_REG_EXP);
+        Pattern numPattern = Pattern.compile(NUM_REG_EXP);
+        final int TAIL_INDEX = 1;
+        final String VALUE = "value";
+        final int ROUNDING_VALUE = 1000000;
+
+        double numResult = 0.0;
+        int errorLines = 0;
+
         while (keys.hasMoreElements()) {
             String key = keys.nextElement();
-            try {
-                Pattern patter = Pattern.compile("index.*");
-                Matcher matcher = patter.matcher(key);
-                if (matcher.matches()) {
-                    String indexKey = matcher.group();
-                    String indexKeyNum = getIndexKeyNum(indexKey);
-                    String indexValue = getIndexValue(rb.getString(indexKey).trim());
-                    String valueNum = rb.getString("value" + indexKeyNum + indexValue);
-                    numResult += Double.parseDouble(valueNum);
+            Matcher keyMatcher = keyPattern.matcher(key);
+            if (keyMatcher.matches()) {
+                String iStr = keyMatcher.group(TAIL_INDEX);
+                String jStr = rb.getString(key).trim();
+                Matcher iMatcher = numPattern.matcher(iStr);
+                Matcher jMatcher = numPattern.matcher(jStr);
+                if (iMatcher.matches() && jMatcher.matches()) {
+                    String valueIJ = VALUE + iStr + jStr;
+                    try {
+                        numResult += Double.parseDouble(rb.getString(valueIJ).trim());
+                    } catch (NumberFormatException | MissingResourceException e) {
+                        errorLines++;
+                    }
+                } else {
+                    errorLines++;
                 }
-            } catch (IllegalArgumentException | MissingResourceException e) {
-                errorLines++;
             }
         }
-        numResult = Math.round(numResult * 1000000);
-        numResult /= 1000000;
+        numResult = Math.round(numResult * ROUNDING_VALUE);
+        numResult /= ROUNDING_VALUE;
         strResult.append(RESULT).append(numResult);
         return errorLines;
     }
 
-    private static String getIndexKeyNum(String key) {
-        Pattern patter = Pattern.compile("index([1-9]\\d*)");
-        Matcher matcher = patter.matcher(key);
-        if (matcher.matches()) {
-            return matcher.group(1);
-        }
-        throw new IllegalArgumentException();
-    }
-
-    private static String getIndexValue(String indexValue) {
-        Pattern patter = Pattern.compile("[1-9]\\d*");
-        Matcher matcher = patter.matcher(indexValue);
-        if (matcher.matches()) {
-            return matcher.group();
-        }
-        throw new IllegalArgumentException();
-    }
-
-    @Test(expected = MissingResourceException.class)
-    public void testNoFile() {
-        int errorLine = getResult("in9", new StringBuilder());
-    }
-
     @Test
     public void getResultFirst() {
+
         final String EXP_RES1 = RESULT + "8.24";
         StringBuilder strResult = new StringBuilder();
         int errorLine = getResult("in1", strResult);
         Assert.assertEquals(3, errorLine);
         Assert.assertEquals(EXP_RES1, strResult.toString());
-    }
 
-    @Test
-    public void getResultSecond() {
         final String EXP_RES2 = RESULT + "30.242";
-        StringBuilder strResult = new StringBuilder();
-        int errorLine = getResult("in2", strResult);
-        Assert.assertEquals(9, errorLine);
-        Assert.assertEquals(EXP_RES2, strResult.toString());
-    }
+        StringBuilder strResult1 = new StringBuilder();
+        int errorLine1 = getResult("in2", strResult1);
+        Assert.assertEquals(9, errorLine1);
+        Assert.assertEquals(EXP_RES2, strResult1.toString());
 
-    @Test
-    public void getResultThird() {
         final String EXP_RES3 = RESULT + "1.9";
-        StringBuilder strResult = new StringBuilder();
-        int errorLine = getResult("in3", strResult);
-        Assert.assertEquals(0, errorLine);
-        Assert.assertEquals(EXP_RES3, strResult.toString());
+        StringBuilder strResult2 = new StringBuilder();
+        int errorLine2 = getResult("in3", strResult2);
+        Assert.assertEquals(0, errorLine2);
+        Assert.assertEquals(EXP_RES3, strResult2.toString());
+
+
+
     }
 }
+
+
+
+
+
