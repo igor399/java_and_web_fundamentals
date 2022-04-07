837b6ffc37cb9a47c01484cf0732b3e3d5af8c91
Igor <88189374+igor399@users.noreply.github.com>
2022-04-07 14:01:06 +0300

    some ref

--- src/by/epam/lab/CurrencyConverter.java
+++ src/by/epam/lab/CurrencyConverter.java
@@ -1,8 +1,5 @@
 package by.epam.lab;
 
 public class CurrencyConverter {
-    private final static String REG_EXP = "%d.%02d";
-    public static String toByn(int coins) {
-        return String.format(REG_EXP, coins / 100, coins % 100);
-    }
+
 }
