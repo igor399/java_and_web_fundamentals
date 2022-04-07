9c2ef175c98f1f7a3a7f87f4b0b569e1fb389ebf
Igor <88189374+igor399@users.noreply.github.com>
2022-04-07 14:02:11 +0300

    some ref

---
+++ src/by/epam/lab/PurchaseFactory.java
@@ -0,0 +1,35 @@
+package by.epam.lab;
+
+public class PurchaseFactory {
+    private final static String SEMICOLON = ";";
+    private final static int PARAM_NUMB = 3;
+
+    private enum PurchaseKind {
+        GENERAL_PURCHASE {
+            @Override
+            Purchase getPurchase(String[] param) {
+                return new Purchase(param);
+            }
+        },
+        PRICE_DISCOUNT_PURCHASE {
+            @Override
+            Purchase getPurchase(String[] param) {
+                return new PriceDiscountPurchase(param);
+            }
+        };
+
+        abstract Purchase getPurchase(String[] param);
+    }
+
+    public static Purchase getPurchaseFromFactory(String csvLine) throws LineException {
+        String[] param = csvLine.split(SEMICOLON);
+
+            return getPurchaseKind(param.length).getPurchase(param);
+
+        }
+
+
+    private static PurchaseKind getPurchaseKind(int paramLength) {
+        return PurchaseKind.values()[paramLength - PARAM_NUMB];
+    }
+}
