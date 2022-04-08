08b3242b7bd22d88cc222cde8a8cb35460576e01
Igor <88189374+igor399@users.noreply.github.com>
2022-04-08 15:17:44 +0300

    some ref

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -31,8 +31,6 @@ public class TestRunner {
         Assert.assertEquals(stringPurchase, purchaseList.toString());
     }
 
-
-
     @Test
     public void testInsertIndex() {
         final int INDEX = 3;
