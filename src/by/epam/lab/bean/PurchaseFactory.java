251ba885f687c54174d5d374d51504c79396ae18
Igor <88189374+igor399@users.noreply.github.com>
2022-04-08 15:55:08 +0300

    some ref

---
+++ src/by/epam/lab/bean/PurchaseFactory.java
@@ -0,0 +1,38 @@
+package by.epam.lab.bean;
+
+import by.epam.lab.exception.LineException;
+
+public class PurchaseFactory {
+    private final static String SEMICOLON = ";";
+    private final static int PARAM_NUMB = 3;
+
+    private enum PurchaseKind {
+        GENERAL_PURCHASE {
+            @Override
+            Purchase getPurchase(String[] param) {
+                return new Purchase(param);
+            }
+        },
+        PRICE_DISCOUNT_PURCHASE {
+            @Override
+            Purchase getPurchase(String[] param) {
+                return new PriceDiscountPurchase(param);
+            }
+        };
+
+        abstract Purchase getPurchase(String[] param);
+    }
+
+    public static Purchase getPurchaseFromFactory(String csvLine) throws LineException {
+        String[] param = csvLine.split(SEMICOLON);
+        try {
+            return getPurchaseKind(param.length).getPurchase(param);
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            throw new LineException(csvLine);
+        }
+    }
+
+    private static PurchaseKind getPurchaseKind(int paramLength) {
+        return PurchaseKind.values()[paramLength - PARAM_NUMB];
+    }
+}
