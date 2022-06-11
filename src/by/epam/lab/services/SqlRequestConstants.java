5b3aeffcaa6771a7e231933843108e7d07486ea0
Igor <88189374+igor399@users.noreply.github.com>
2022-06-11 21:26:05 +0300

    some ref

--- src/by/epam/lab/services/SqlRequestConstants.java
+++ src/by/epam/lab/services/SqlRequestConstants.java
@@ -2,7 +2,7 @@ package by.epam.lab.services;
 
 public final class SqlRequestConstants {
     public static final String INSERT_RESULT = "INSERT INTO results (loginId, " +
-            "testId, date, mark) VALUES(?, ?, ?, ?)";
+            "testId, dat, mark) VALUES(?, ?, ?, ?)";
     public static final String INSERT_LOGIN = "INSERT INTO logins (name) VALUES (?)";
     public static final String INSERT_TEST = "INSERT INTO tests (name) VALUES (?)";
     public static final String SELECT_ID_LOGIN = "SELECT idLogin FROM logins " +
@@ -16,7 +16,7 @@ public final class SqlRequestConstants {
             "BY logins.idLogin";
 
     public static final String CURR_MONTH_RESULTS = "SELECT logins.name, tests.name, " +
-            "results.date, results.mark FROM logins, tests, results WHERE " +
+            "results.dat, results.mark FROM logins, tests, results WHERE " +
             "results.loginId = logins.idLogin AND results.testId = tests.idTest " +
-            "AND MONTH(results.date) = MONTH(CURRENT_DATE()) ORDER BY results.date";
+            "AND MONTH(results.dat) = MONTH(CURRENT_DATE()) ORDER BY results.dat";
 }
