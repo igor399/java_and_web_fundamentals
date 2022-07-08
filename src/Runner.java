a35795a5d77227105956ccdbb1a9ff2945ec5e3e
Igor <88189374+igor399@users.noreply.github.com>
2022-07-08 11:26:58 +0300

    some ref

---
+++ src/Runner.java
@@ -0,0 +1,27 @@
+import by.epam.lab.beans.Drop;
+import by.epam.lab.exceptions.CustomInterruptedException;
+import by.epam.lab.services.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+import static by.epam.lab.services.GlobalConstants.*;
+
+public class Runner {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(new FileReader(FILE_DIRECTORY))) {
+            Drop drop = new Drop();
+            Thread thread1 = new Thread(new Producer(drop, scanner));
+            Thread thread2 = new Thread(new Consumer(drop));
+            thread1.start();
+            thread2.start();
+            thread1.join();
+        } catch (FileNotFoundException e) {
+            System.err.println(NO_FILE);
+        }catch (InterruptedException e ){
+            throw new CustomInterruptedException(String.format
+                    (THREAD_ERROR, Thread.currentThread().getName()));
+        }
+    }
+}
---
+++ src/by/epam/lab/exceptions/CustomInterruptedException.java
@@ -0,0 +1,20 @@
+package by.epam.lab.exceptions;
+
+public class CustomInterruptedException extends RuntimeException {
+
+    public CustomInterruptedException() {
+        super();
+    }
+
+    public CustomInterruptedException(String message) {
+        super(message);
+    }
+
+    public CustomInterruptedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomInterruptedException(Throwable cause) {
+        super(cause);
+    }
+}
