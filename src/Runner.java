d9502681cd30b4f889672e90e31eadd3a15a839f
Igor <88189374+igor399@users.noreply.github.com>
2022-07-22 21:19:53 +0300

    some ref

--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,36 @@
 import by.epam.lab.services.*;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import static by.epam.lab.services.GlobalConstants.*;
 
 public class Runner {
     public static void main(String[] args) {
 
+        try(FileReader fileReader = new FileReader("resources/properties/settings.properties")) {
+            Properties properties = new Properties();
+            properties.load(fileReader);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/by/epam/lab/services/TrialWriter.java
+++ src/by/epam/lab/services/TrialWriter.java
@@ -1,4 +1,23 @@
 package by.epam.lab.services;
 
-public class TrialWriter {
+import by.epam.lab.beans.Trial;
+
+import java.util.Deque;
+
+public class TrialWriter implements Runnable{
+    private final Deque<Trial> trialBuffer;
+    private final String path;
+
+    public TrialWriter(Deque<Trial> trialBuffer, String path) {
+        this.trialBuffer = trialBuffer;
+        this.path = path;
+    }
+
+
+    @Override
+    public void run() {
+
+
+
+    }
 }
