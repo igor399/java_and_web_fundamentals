7eab1366eb445d9c09b494a6b0737b11ad94546e
Igor <88189374+igor399@users.noreply.github.com>
2022-07-22 19:29:19 +0300

    some ref

--- src/by/epam/lab/services/TrialConsumer.java
+++ src/by/epam/lab/services/TrialConsumer.java
@@ -2,13 +2,21 @@ package by.epam.lab.services;
 
 import by.epam.lab.beans.*;
 
+import java.util.Deque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import static by.epam.lab.services.GlobalConstants.*;
 
 public class TrialConsumer implements Runnable {
-    private final TrialBuffer trialBuffer;
+    private final BlockingQueue<String> stringsBuffer;
+    private final Deque<Trial> trialBuffer;
+    private final AtomicBoolean isDone;
 
-    public TrialConsumer(TrialBuffer trialBuffer) {
+    public TrialConsumer(BlockingQueue<String> stringsBuffer, Deque<Trial>trialBuffer, AtomicBoolean isDone) {
+        this.stringsBuffer = stringsBuffer;
         this.trialBuffer = trialBuffer;
+        this.isDone = isDone;
     }
 
     @Override
