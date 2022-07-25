464de6c2582f198c82839c2a55e4c8988d313e5a
Igor <88189374+igor399@users.noreply.github.com>
2022-07-25 15:58:11 +0300

    some ref

--- src/by/epam/lab/services/GlobalConstants.java
+++ src/by/epam/lab/services/GlobalConstants.java
@@ -5,7 +5,6 @@ import by.epam.lab.beans.Trial;
 public class GlobalConstants {
     public final static String SEMICOLON = ";";
     public final static String NO_FILE = "File not found";
-    public final static String THREAD_ERROR = "Thread error: %s";
     public final static int PASS_MARK = 120;
     public final static int ACCOUNT_IND = 0;
     public final static int MARK1_IND = 1;
@@ -17,6 +16,7 @@ public class GlobalConstants {
     public final static String PROPERTIES_DIRECTORY = "resources/settings.properties";
     public final static String FOLDER_NAME = "folderName";
     public final static String RESULT_FILE = "resultName";
+
 
 
 }
