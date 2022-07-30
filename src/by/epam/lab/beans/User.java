c7af5130b44da9db81013e521de4d6f9573293dc
Igor <88189374+igor399@users.noreply.github.com>
2022-07-30 17:34:43 +0300

    some ref

--- src/by/epam/lab/beans/User.java
+++ src/by/epam/lab/beans/User.java
@@ -3,6 +3,9 @@ package by.epam.lab.beans;
 public class User {
     private int id;
     private String account;
-    
-    
+
+    public User(int id, String account) {
+        this.id = id;
+        this.account = account;
+    }
 }
