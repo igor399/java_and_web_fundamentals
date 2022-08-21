4a13fe6543ea6d1ed2b04bacd424cb4152eeb4e0
Igor <88189374+igor399@users.noreply.github.com>
2022-08-21 13:19:24 +0300

    some ref
    Signed-off-by: Igor <88189374+igor399@users.noreply.github.com>

--- src/main/java/by/epam/lab/controller/impl/MemoryImplNumberDAO.java
+++ src/main/java/by/epam/lab/controller/impl/MemoryImplNumberDAO.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 public class MemoryImplNumberDAO implements NumberDAO {
 
-    private final List<Double> numbers = Arrays.asList(1.0, 2.3, 3.5);
+    private final List<Double> numbers = Arrays.asList(1.22, 33.65, -100.12, 0.5,
+            8.0, 29.0, 500.5, 900.9, 123.14, 77.88);
 
     @Override
     public List<Double> getNumbers() {
